56fd1b59130078aba4064874daae6478ec1b33ba
Deafterfeather <sugeng.kcs@gmail.com>
2024-07-23 13:51:40 +0800

    upadate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,9 +7,42 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()  
+    public function index()
     {
         $posts = Post::latest()->paginate(3);
         return view('posts.index', compact('posts'));
+    }
+
+    //Mmebuat function crate untuk menampilkan form tambah data
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    //Membuat function store untuk memperoses data ke database dan upload gambar
+    public function  store(request $request)
+    {
+        //Membuat validasi form
+        $this->validate($request, [
+            'image'    => 'required|image|mimes:,jpeg|max:2040',
+            'title'   => 'required|min:5',
+            'content'  => 'required|min:10'
+        ]);
+
+        //Upload gambar
+        $image = $request->file('image');
+        $image->storeAs('public/posts',$image->hashName());
+
+        //simpan ke database
+        Post::create([
+            'image'     =>$image->hashName(),
+            'title'    => $request->title,
+            'content'  => $request->content
+        ]);
+
+        //redirect ke halaman index
+        return redirect()->route('posts.index')->with([
+            'success' => 'Data Berhasil disimpan'
+        ]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
     <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
   </head>
   <body style="background: lightgray">
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
             <div class="col">
                 <div class="">
@@ -29,15 +29,16 @@
                             <tbody>
                                 @forelse ($posts as $post )
                                 <tr>
-                                    <td class="rext-center">
-                                        <img src="{{ assset('/storage/posts/'.$post->image) }}" class="rounded" style="150px" alt="">
+                                    <td class="text-center">
+                                        <img src="{{ asset('/storage/posts/' . $post->image) }}" class="rounded"
+                                            style="150px" alt="">
                                     </td>
                                     <td>{{ $post->title }}</td>
                                     <td>{{ $post->content }}</td>
                                     <td class="text-center">
                                         <form action="{{ route('posts.destroy' ,$post->id) }}" method="POST" onclick="return confirm('Apakah anda yakin ?')">
                                             <a href="{{ route('posts.show' , $post->id) }}" class="btn btn-dark">View</a>
-                                            <a href="{{ route('posts.eidt' , $post->id) }}" class="btn btn-warning">Edit</a>
+                                            <a href="{{ route('posts.edit' , $post->id) }}" class="btn btn-warning">Edit</a>
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-sm btn-danger">Delete</button>
@@ -60,15 +61,18 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <script>
-        @if (session()->has('success'))
-
-        toastr.succes('{{ session('success') }}','BERHASIL!');
-        @elseif (session()->has('error'))
-
-        toastr.error('{{ session('error') }}', 'GAGAL!');
-
-        @endif
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @if(session('success'))
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Data Berhasil DIsimpan',
+                    timer: 3000,
+                    showConfirmButton: false
+                });
+            });
+        </script>
+    @endif
   </body>
 </html>
